cc2f937090dd44b93fde8065403c1e3c3a6f81ed
camikazegreen <camikazegreen@gmail.com>
2013-02-09 10:53:54 -0700

    Adding functionality to date-picker.
    Added the jQuery scroll to the datepicker.
    Added the ability to view the past on the datepicker.
    Added the ability to jump to points in the date picker from the top
    month nav bar (Only if they already exist.)

--- js/calendar.js
+++ js/calendar.js
@@ -1,4 +1,13 @@
-var startDate
+$(document).ready(function() {
+   $.localScroll({
+   	duration:800,
+   	target:'#datepicker',
+   	lazy:true,
+   	hash:false
+   	});
+});
+	var lastPixelTop = "";
+	var startDate
 if (Date.today().is().sunday()){startDate = Date.today().last().sunday().addDays(-1)} 
 else {startDate = Date.today().last().week().last().sunday().addDays(-1)}
 
@@ -9,7 +18,7 @@ function createWeek(startDate){
 	var weekID = yearNumber+'week'+weekNumber;
 	week.setAttribute('id', weekID);
 	document.getElementById('calendarTable').appendChild(week);
-	document.getElementById('yearDiv').innerHTML = yearNumber;
+	// document.getElementById('yearDiv').innerHTML = yearNumber;
 	var i = 0;
 	while (i<7){
 		var date = startDate.addDays(1); 
@@ -17,28 +26,35 @@ function createWeek(startDate){
 		var dayName = date.toString('dddd');
 		var monthNumber = date.toString('MM');
 		var monthName = date.toString('MMM');
+		yearNumber = date.toString('yyyy');
 		function isOdd(num) { return num % 2;};
 		var monthClass = 'evenMonth'
 		if (isOdd(monthNumber*1)) {
 			monthClass = 'oddMonth';
 		};
+		var fullDate = yearNumber+'-'+monthNumber+'-'+dayNumber;
 		var day = document.createElement('td');
 		var dayClass = dayName+' picker-day '+monthClass;
 				if (Date.today().compareTo(date)==0) {
 					dayClass = dayClass+' today';
+					document.getElementById('todaylink').setAttribute('href','#'+fullDate);
 				};
 				if (Date.today().compareTo(date)==1) {
 					dayClass = dayClass+' past-day';
 				}
 		day.setAttribute('class',dayClass);
-		day.setAttribute('id',yearNumber+'-'+monthNumber+'-'+dayNumber);
+		day.setAttribute('id',fullDate);
+		var linkAnchor = document.createElement('a');
+		linkAnchor.setAttribute('name',fullDate);
 		var dayDiv = document.createElement('div');
 		dayDiv.setAttribute('class','dayDiv');
 		var monDiv = document.createElement('div');
 		monDiv.setAttribute('class','monDiv');
 		document.getElementById(weekID).appendChild(day);
-		document.getElementById(yearNumber+'-'+monthNumber+'-'+dayNumber).appendChild(monDiv);
-		document.getElementById(yearNumber+'-'+monthNumber+'-'+dayNumber).appendChild(dayDiv);
+		var dateBox = document.getElementById(fullDate)
+		dateBox.appendChild(linkAnchor);
+		dateBox.appendChild(monDiv);
+		dateBox.appendChild(dayDiv);
 		monDiv.innerHTML = monthName;
 		dayDiv.innerHTML = dayNumber;
 		i++;
@@ -53,19 +69,94 @@ function createMonth(startDate){
 	}
 }
 
+function createPastWeek(){
+	var earliestWeek = document.getElementById('calendarTable').firstChild;
+	var earliestDayID = document.getElementById('calendarTable').firstChild.firstChild.id; //get the date of the first child day of the first child week of the calendarTable
+	var earliestDay = Date.parse(earliestDayID);
+	var pastStartDate = earliestDay.last().sunday().addDays(-1)
+	// document.getElementById('yearDiv').innerHTML = pastStartDate;
+
+	var weekNumber = pastStartDate.getISOWeek();
+	var yearNumber = pastStartDate.toString('yyyy');
+	var week = document.createElement('tr');
+	var weekID = yearNumber+'week'+weekNumber;
+	week.setAttribute('id', weekID);
+	// $('#datepicker').prepend(week);
+	// document.getElementById('calendarTable').insertRowBefore(0, week);
+
+	earliestWeek.parentNode.insertBefore(week, earliestWeek);
+	// document.getElementById('calendarTable').appendChild(week);
+
+	var i = 0;
+	while (i<7){
+		var date = pastStartDate.addDays(1); 
+		var dayNumber = date.toString('dd');
+		var dayName = date.toString('dddd');
+		var monthNumber = date.toString('MM');
+		var monthName = date.toString('MMM');
+		yearNumber = date.toString('yyyy');
+		function isOdd(num) { return num % 2;};
+		var monthClass = 'evenMonth'
+		if (isOdd(monthNumber*1)) {
+			monthClass = 'oddMonth';
+		};
+		var fullDate = yearNumber+'-'+monthNumber+'-'+dayNumber;
+		var day = document.createElement('td');
+		var dayClass = dayName+' picker-day '+monthClass;
+				if (Date.today().compareTo(date)==0) {
+					dayClass = dayClass+' today';
+					document.getElementById('todaylink').setAttribute('href','#'+fullDate);
+				};
+				if (Date.today().compareTo(date)==1) {
+					dayClass = dayClass+' past-day';
+				}
+		day.setAttribute('class',dayClass);
+		day.setAttribute('id',fullDate);
+		var linkAnchor = document.createElement('a');
+		linkAnchor.setAttribute('name',fullDate);
+		var dayDiv = document.createElement('div');
+		dayDiv.setAttribute('class','dayDiv');
+		var monDiv = document.createElement('div');
+		monDiv.setAttribute('class','monDiv');
+		document.getElementById(weekID).appendChild(day);
+		var dateBox = document.getElementById(fullDate)
+		dateBox.appendChild(linkAnchor);
+		dateBox.appendChild(monDiv);
+		dateBox.appendChild(dayDiv);
+		monDiv.innerHTML = monthName;
+		dayDiv.innerHTML = dayNumber;
+		i++;
+	}
+}
+
+function initialPastMonth(){
+	var i=0;
+	while(i<4){
+		createPastWeek();
+		i++;
+	}
+	var xstartDate
+if (Date.today().is().sunday()){xstartDate = Date.today().last().sunday()} 
+else {xstartDate = Date.today().last().week().last().sunday()}
+	// window.location.hash = xstartDate.toString('yyyy')+'-'+xstartDate.toString('MM')+'-'+xstartDate.toString('dd');
+	window.location.hash = Date.today().toString('yyyy')+'-'+Date.today().toString('MM')+'-'+Date.today().toString('dd');
+}
 function pageDatepicker(){
 	var pixels = $("#calendarTable").height();
 	var pixelTop = $("#datepicker").scrollTop();
+	document.getElementById('yearDiv').innerHTML= 'pixels = '+pixels+' pixelTop = '+pixelTop
 	 if (pixels-pixelTop*1<220) {
-		// document.getElementById('pixelCount').innerHTML = pixels+', '+pixelTop;
-		// alert(pixels);
 		createMonth(startDate)
 	};
+	if (pixelTop*1<4 && pixelTop<lastPixelTop){
+		createPastWeek();
+	}
+	lastPixelTop = pixelTop;
 }
 
 function createYear(){
 	var monthBox = document.getElementById('monthList'); //get the container
-	 var startMonth = Date.today().add(-3).months();//get the current month minus four to start.
+	 var startMonth = Date.today().add(-3).months();//get the current month minus three.
 	var k = 0;
 	while (k<12){
 		var month = document.createElement('li'); //create a new list item
@@ -75,7 +166,10 @@ function createYear(){
 		if (startMonth.toString('MMM')==Date.today().toString('MMM')){
 		month.setAttribute('class','key today');
 		} //check if it's this month and apply active class
-		monthBox.appendChild(month);
+		var monthLink = document.createElement('a');
+		monthLink.setAttribute('href','#'+startMonth.toString('yyyy')+'-'+startMonth.toString('MM')+'-01')
+		monthLink.appendChild(month);
+		monthBox.appendChild(monthLink);
 		startMonth.addMonths(1); //add a month to current month
 		k++;
 	}
